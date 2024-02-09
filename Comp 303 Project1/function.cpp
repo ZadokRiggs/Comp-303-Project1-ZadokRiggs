47eda4df67b1f79d3df3da930b8c80bf06b9ea82
ZadokRiggs <143667821+ZadokRiggs@users.noreply.github.com>
2024-02-08 23:55:56 -0600

    Update function.cpp

--- Comp 303 Project1/function.cpp
+++ Comp 303 Project1/function.cpp
@@ -1,13 +1,47 @@
 #include "function.h"
-int check_num(int num) {
+#include <iostream>
+using namespace std;
 
+int check_num(int &size, int array[], int num) {
+	for (int i = 0; i < size; i++) {
+		if (array[i] == num) {
+			return i;
+		}
+	}
+	return -999;
 }
-int modify(int num) {
-
+int modify(int &size, int array[], int num, int num2) {
+	//replaces space at num with num2
+	if (num < size && num >= 0) {
+		array[num] = num2;
+		return 1;
+	}
+	return 0;
+	
 }
-void append(int num) {
-
+void append(int &size, int array[], int num) {
+	size += 1;
+	int* new_data = new int[size];
+	for (int i = 0; i < size; i++) {
+		new_data[i] = array[i];
+		cout << i << " ";
+	}
+	cout << 3 << endl;
+	new_data[size-1] = num;
+	cout << 2 << endl;
+	delete[] array;
+	cout << 1 << endl;
+	array = new_data;
+	cout << endl 
+		<< "Append Successful." 
+		<< endl 
+		<< endl;
+	return;
 }
-void zero_out(int num) {
-
+int zero_out(int &size, int array[], int num) {
+	if (num < size && num >= 0) {
+		array[num] = 0;
+		return 1;
+	}
+	return 0;
 }
